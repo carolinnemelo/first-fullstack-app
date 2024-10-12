297e15e13db735fd3b1fe710834e39d5cbaec4b0
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-12 17:28:34 +0200

    create firt zero test

---
+++ backend/src/lib.test.ts
@@ -0,0 +1,8 @@
+import { deepEqual } from "node:assert/strict";
+import test from "node:test";
+
+test("Should throw error when imputed zero numbers", () => {
+  const amountOfChars = 0;
+  const result = passwordGenerator(amountOfChars)
+  deepEqual(result, "zero numbers");
+});
