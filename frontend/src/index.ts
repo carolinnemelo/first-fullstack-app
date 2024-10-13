51ed9759aea4d95246178085f885b18526f0d7d5
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-13 12:17:54 +0200

    getting and logging inputed amount of chars

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,11 +1,14 @@
 const form = document.querySelector("#keyForm");
+const amountOfCharsElement = document.querySelector("#amountOfCharacters") as HTMLFormElement;
 
-// const generateKey = async (amountOfChars: number) => {
-//     const fetchKey = await fetch("http://localhost:3000");
-//     console.log(fetchKey);
-// }
+const generateKey = async (amountOfChars: number) => {
+  const fetchKey = await fetch("http://localhost:3000");
+  console.log(fetchKey);
+};
 
 form?.addEventListener("submit", (event) => {
   event.preventDefault();
+  const amountOfChars = amountOfCharsElement.value;
+  console.log(amountOfChars)
   console.log("submitted from addEventListener");
 });
