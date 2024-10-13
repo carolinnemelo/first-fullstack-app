8e6b48cf7d8c59182ec8b66b6840f4306395ed4b
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-13 16:07:23 +0200

    get element and display key

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -3,20 +3,24 @@ const amountOfCharsElement = document.querySelector(
   "#amountOfCharacters",
 ) as HTMLFormElement;
 
+const displayKeyElement = document.querySelector(
+  "#displayKey"
+) as HTMLParagraphElement;
+
 const requestGeneratedKey = async (amountOfChars: number) => {
   const fetchKey = await fetch("http://localhost:3000", {
     method: "post",
     body: `${amountOfChars}`,
   });
   const data = await fetchKey.text();
-  console.log(data)
-  return data
+  return data;
 };
 
 form?.addEventListener("submit", async (event) => {
   event.preventDefault();
   const amountOfChars = amountOfCharsElement.value;
   const generatedKey = await requestGeneratedKey(amountOfChars);
-    
+  displayKeyElement.textContent = generatedKey;
+
   //   console.log("submitted from addEventListener");
 });
